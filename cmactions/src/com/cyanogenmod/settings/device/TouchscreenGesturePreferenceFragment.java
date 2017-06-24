d87a608258fbffdd8f646d3f835c44fb59c3e2d2
xenreon <souradeep999@gmail.com>
2017-06-24 12:56:28 +0530

    Fix cmactions (based on potter commit)

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
@@ -29,11 +29,6 @@ import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.text.TextUtils;
 
-import java.io.File;
-
-import org.cyanogenmod.internal.util.FileUtils;
-import org.cyanogenmod.internal.util.ScreenType;
-
 public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
     private SwitchPreference mFlipPref;
     private NotificationManager mNotificationManager;
@@ -70,33 +65,6 @@ public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
         }
     }
 
-    @Override
-    public void addPreferencesFromResource(int preferencesResId) {
-        super.addPreferencesFromResource(preferencesResId);
-        // Initialize node preferences
-        for (String pref : Constants.sBooleanNodePreferenceMap.keySet()) {
-            SwitchPreference b = (SwitchPreference) findPreference(pref);
-            if (b == null) continue;
-            b.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    String node = Constants.sBooleanNodePreferenceMap.get(preference.getKey());
-                    if (!TextUtils.isEmpty(node)) {
-                        Boolean value = (Boolean) newValue;
-                        FileUtils.writeLine(node, value ? "1" : "0");
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            String node = Constants.sBooleanNodePreferenceMap.get(pref);
-            if (new File(node).exists()) {
-                String curNodeValue = FileUtils.readOneLine(node);
-                b.setChecked(curNodeValue.equals("1"));
-            } else {
-                b.setEnabled(false);
-            }
-        }
-    }
 
     @Override
     public void onResume() {
