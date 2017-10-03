4837c00d484a500856556a59fd838b43903c3723
Keerten <keertenk@gmail.com>
2017-10-03 11:32:21 +0530

    athene : update for camera -> Dont build camera wrapper -> Add lbshims_camera

--- device.mk
+++ device.mk
@@ -114,8 +114,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8952 \
     libbson \
+    libshims_camera \
     Snap \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
