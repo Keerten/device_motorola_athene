8e5f6deb13e0b367d8dcc2d5bdfa9664ae22e5f5
Keerten <keertenk@gmail.com>
2018-01-22 17:11:16 +0000

    athene: PixelExperience Bringup!
    Signed-off-by: Keerten <keertenk@gmail.com>

--- aosp_athene.mk
+++ aosp_athene.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod Project
-#           (C) 2017 The LineageOS Project
+# Copyright (C) 2017 The AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,28 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit some common stuff.
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
 # Boot animation
-
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
-TARGET_BOOTANIMATION_HALF_RES := true
+
+# Inherit some common AOSP stuff.
+TARGET_ARCH := arm64
+TARGET_DENSITY := xxhdpi
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_ARCORE := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := lineage_athene
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := athene
-PRODUCT_MODEL := athene
+PRODUCT_NAME := aosp_athene
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-       PRODUCT_NAME="Moto G4/Plus"
+    PRODUCT_NAME="Moto G4/Plus"
